16f10c07c6949f9d41113134981a591c70409827
Fernando <andresfernandoxd1591@gmail.com>
2024-01-02 12:36:12 -0500

    change template helpersTest

--- test/userModule.test.js
+++ test/userModule.test.js
@@ -1,23 +1,32 @@
 /* eslint-disable no-undef */
 import app from '../src/app.js'
 import request from 'supertest'
-import { updateOneUser } from './helpersTest.js'
+import { registerNewUser, updateOneUser } from './helpersTest.js'
 
 describe('test API user Route', () => {
+  test('POST for "/newUser" route', async () => {
+    const res = await request(app).post('/api/newUser').send(registerNewUser)
+    console.log(res.body)
+    expect(res.statusCode).toBe(200)
+  })
   test('GET for "/allUsers" route', async () => {
     const res = await request(app).get('/api/allUsers').send()
     expect(res.body).toHaveLength(res.body.length)
     expect(res.statusCode).toBe(200)
   })
   test('GET for "/oneUser/:id" route', async () => {
-    const res = await request(app).get('/api/oneUser/41e1dc81-25d1-45a4-acce-3d7896d579a9').send()
-    expect(res.body.name).toEqual('Ricardo')
-    expect(res.body.email).toEqual('felipao6578@gmail.com')
+    const res = await request(app).get('/api/oneUser/:id').send()
+    expect(res.body.name).toEqual('<replace this to BD information>')
+    expect(res.body.email).toEqual('<replace this to BD information>')
     expect(res.statusCode).toBe(200)
   })
   test('PUT for "/putUser/:id" route', async () => {
-    const res = await request(app).put('/api/putUser/ceddf7c0-5157-4c39-8901-1ded88a1443a').send(updateOneUser)
+    const res = await request(app).put('/api/putUser/:id').send(updateOneUser)
     console.log(res.body)
+    expect(res.statusCode).toBe(200)
+  })
+  test('Delete for "/deleteUser/:id" route', async () => {
+    const res = await request(app).delete('/api/deleteUser/:id').send()
     expect(res.statusCode).toBe(200)
   })
 })
