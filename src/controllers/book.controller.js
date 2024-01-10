753aec4c4f19b8daeb37adb4400db046eae58fd7
Fernando <andresfernandoxd1591@gmail.com>
2024-01-10 11:45:02 -0500

    add book, restrict actions by role

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -2,10 +2,10 @@ import { prisma } from '../db.js'
 
 export const newBook = async (req, res) => {
   try {
-    const { doctorAssignement } = req.body
+    const { doctorDoc, startMonthBook, endMonthBook, yearBook } = req.body
     const user = await prisma.user.findUnique({
       where: {
-        docUser: doctorAssignement,
+        docUser: doctorDoc,
         AND: {
           role: 'ODONTOLOGO',
           state: 'ACTIVE'
@@ -19,13 +19,43 @@ export const newBook = async (req, res) => {
     }
     const book = await prisma.book.create({
       data: {
-        dentistId: doctorAssignement
+        dentistId: doctorDoc,
+        startMonth: startMonthBook,
+        endMonth: endMonthBook,
+        year: yearBook
       }
     })
     res.json(book)
   } catch (error) {
     return res.status(500).json({
       message: 'something goes wrong'
+    })
+  }
+}
+
+export const getAllBooks = async (req, res) => {
+  try {
+    const books = await prisma.book.findMany()
+    res.json(books)
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Something goes wrong'
+    })
+  }
+}
+
+export const getOneBook = async (req, res) => {
+  try {
+    const identifier = Number(req.params.id)
+    const book = await prisma.book.findUnique({
+      where: {
+        idBook: identifier
+      }
+    })
+    res.json(book)
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Something goes wrong'
     })
   }
 }
